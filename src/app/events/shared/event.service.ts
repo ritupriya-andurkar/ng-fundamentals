631eeccfdb083bf50d704db6f86f0b7ab88f0136
Ritu <ritu@ritu-HP-Pavilion-Notebook>
2021-02-09 16:59:22 +0530

    routing, accessing route parameters, linking to routes, navigating from code

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class EventService{
-    getEvents(){
-        return EVENTS
-    }
+  getEvents(){
+    return EVENTS
+  }
+
+  getEvent(id: number){
+    return EVENTS.find(event => event.id === id)
+  }
 }
 
 const EVENTS = [
